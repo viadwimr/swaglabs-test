04acba3294792e48a845cb2a5ac794b6ed770861
viadwimr <viadwimr@github.com>
2023-10-22 22:17:19 +0700

    add login test

--- src/test/java/swagLabs/Login.java
+++ src/test/java/swagLabs/Login.java
@@ -1,4 +1,28 @@
 package swagLabs;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
 public class Login {
+    @Test
+    public static void main(String[] args) {
+        WebDriver driver;
+        String baseUrl = "https://www.saucedemo.com/";
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions opt = new ChromeOptions();
+        opt.setHeadless(false);
+        driver = new ChromeDriver(opt);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String pageName = driver.findElement(By.className("title")).getText();
+        Assert.assertEquals(pageName, "Products");
+        // driver.close();
+    }
 }
+
--- src/test/java/swagLabs/LoginFailed.java
+++ src/test/java/swagLabs/LoginFailed.java
@@ -1,4 +1,32 @@
 package swagLabs;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginFailed {
+    @Test
+    public static void main(String[] args) {
+        WebDriver driver;
+        String baseUrl = "https://www.saucedemo.com/";
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions opt = new ChromeOptions();
+        opt.setHeadless(false);
+        driver = new ChromeDriver(opt);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("password-salah");
+        driver.findElement(By.id("login-button")).click();
+        String errorMessage = driver.findElement(By.className("error-button")).getText();
+        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service");
+        // driver.close();
+    }
 }
+
